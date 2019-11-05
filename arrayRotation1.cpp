a3784d57ccd8a05b55390b9ad19b7d8c5703d6a4
bapuAbhijeet <abhijeet15111996@gmail.com>
2019-11-05 05:54:32 +0000

    edit3 arraySize increased

--- arrayRotation1.cpp
+++ arrayRotation1.cpp
@@ -20,7 +20,7 @@ void rotate(int arr[], int size){
 }
 int main()
 {
-    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
     int size = sizeof(arr)/sizeof(arr[0]);
     rotate (arr,size);
     for(int i=0;i<size;i++)
