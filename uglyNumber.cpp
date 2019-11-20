4958fb8e56b4ac0a38901061da35bb4ab370c36a
bapuAbhijeet <abhijeet15111996@gmail.com>
2019-11-20 12:13:24 +0000

    edit 2

--- uglyNumber.cpp
+++ uglyNumber.cpp
@@ -16,13 +16,41 @@ int isUgly(int no)
     
   return (no == 1)? 1 : 0; 
 } 
+void primeFactors(int n)
+{
+     while (n % 2 == 0)  
+    {  
+        cout << 2 << " ";  
+        n = n/2;  
+    } 
+    for (int i = 3; i <= sqrt(n); i = i + 2)  
+    {  
+         while (n % i == 0)  
+        {  
+            cout << i << " ";  
+            n = n/i;  
+        }  
+    } 
+    if (n > 2)  
+        cout << n << " ";
+}
 void getUglyNumbers(int n)
 {
     int i=1;
     while(i<n)
     {
         if(isUgly(i))
-            cout << i << "\n";
+        {  
+            cout << i << "=" ;
+            primeFactors(i);
+            cout <<"\n";
+        }
+        else
+        {
+        cout << i << "=" ;
+        primeFactors(i);
+        cout <<"\n";
+        }
         i++;
     }
 }
