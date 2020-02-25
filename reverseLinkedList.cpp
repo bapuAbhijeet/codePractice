1d260597c115c50df6d70b244971bd07b2fc72e8
bapuAbhijeet <abhijeet15111996@gmail.com>
2020-02-25 08:47:51 +0000

    created node for linked list

---
+++ reverseLinkedList.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    node * next;
+    node (int n)
+    {
+        this->data = n;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
