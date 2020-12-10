a55a79020dd0d8793200ec1c78183b3c52db9a6a
bapuAbhijeet <abhijeet15111996@gmail.com>
2020-12-10 10:30:05 +0000

    2nd commit

--- Dates.java
+++ Dates.java
@@ -1,30 +1,31 @@
+import java.util.Calendar;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.*; 
 
 public class Dates{
-   public static void main(String args[]){
-	 SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
-	 String addonAdditionDate1 = "02 02 2020";
-	 String currentDate1 = "27 11 2020";
+   public static void main(final String args[]) {
+       final SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
+       final String addonAdditionDate1 = "02 02 2020";
+       final String currentDate1 = "27 11 2020";
 
-	 try {
-	       Date addonAdditionDate = myFormat.parse(addonAdditionDate1);
-	       Date currentDate = myFormat.parse(currentDate1);
-	       long difference = currentDate.getTime() - addonAdditionDate.getTime();
-	       //float daysBetween = (difference / (1000*60*60*24));
-               /* You can also convert the milliseconds to days using this method
-                * float daysBetween = 
-                *         TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS)
-                */
-            float daysBetween = TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
-            float N = daysBetween / 29;
-	       System.out.println("Number of Days between dates: "+daysBetween);
-	       System.out.println("N : "+ N);
-	       System.out.println("Ceil : " + Math.ceil(N));
-	       //Date newRecurrenceDate = addonAdditionDate + (N*29);
-	       //System.out.println("New Recurrence Date: "+newRecurrenceDate);
-	 } catch (Exception e) {
+       try {
+           final Date addonAdditionDate = myFormat.parse(addonAdditionDate1);
+           final Date currentDate = myFormat.parse(currentDate1);
+           final long difference = currentDate.getTime() - addonAdditionDate.getTime();
+           final float daysBetween = (difference / (1000 * 60 * 60 * 24));
+           final float N = daysBetween / 29;
+           System.out.println("Number of Days between dates: " + daysBetween);
+           System.out.println("N : " + N);
+           int number = (int)Math.ceil(N);
+           System.out.println("Ceil : " + number);
+           final Calendar c = Calendar.getInstance();
+           c.setTime(addonAdditionDate);
+           c.add(Calendar.DATE, (number * 29));
+           // Date newRecurrenceDate = addonAdditionDate.plus;
+           Date newRecurrenceDate= c.getTime();
+           System.out.println("New Recurrence Date: "+newRecurrenceDate);
+       } catch (final Exception e) {
 	       e.printStackTrace();
 	 }
    }
