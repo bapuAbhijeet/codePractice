9d2b19b8c7014c50d32c97d765a3e8964e51153f
bapuAbhijeet <abhijeet15111996@gmail.com>
2021-05-12 16:58:31 +0000

    date formaters

--- Operators.java
+++ Operators.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 public class Operators{
    public static void main(String[] args)
     {
@@ -12,5 +15,21 @@ public class Operators{
 
         sentence = sentence.replaceAll("\\s", "");
         System.out.println("After replacement: " + sentence);
+
+
+//https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+        String string = "2017-01-01";
+        LocalDate date = LocalDate.parse(string, DateTimeFormatter.ISO_DATE);
+        System.out.println(date);
+
+        string = "August 25, 2017";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.ENGLISH);
+        date = LocalDate.parse(string, formatter);
+        System.out.println(date);
+
+        string = "August 25, 2017";
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate("MMMM d, yyyy");
+        date = LocalDate.parse(string, formatter);
+        System.out.println(date);
     }
 }
